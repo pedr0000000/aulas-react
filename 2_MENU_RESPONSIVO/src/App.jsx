c2cc2b2d24adc0d653c77f628a59000d401c8bc3
Pedro <pedro.santos0908@eaportal.org>
2023-09-22 15:47:14 -0300

    projeto finalizado

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -4,8 +4,13 @@ import Header from "./Components/Header"
 
 function App() {
   return (
+
+    <div>
    <Header/>
+   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas quo mollitia alias natus numquam commodi, recusandae incidunt praesentium impedit temporibus dicta aliquam enim voluptatem ratione animi, repellendus in hic omnis.</p>
+   </div>
   )
+
 }
 
 export default App
--- 2_MENU_RESPONSIVO/src/Components/Header.jsx
+++ 2_MENU_RESPONSIVO/src/Components/Header.jsx
@@ -1,26 +1,61 @@
-import { List } from "@phosphor-icons/react"
+import { List, X } from "@phosphor-icons/react"
+import { useState } from "react"
 
 import styles from "./Header.module.css"
 
 function Header() {
+  const [abrirMenu, setAbrirMenu] = useState(false)
+
+  function handleAbrirFecharMenu() {
+    if (abrirMenu === true) {
+      setAbrirMenu(false)
+      return
+    }
+    setAbrirMenu(true)
+
+  }
   return (
     <header>
-        <div className="mobile">
-            <div className={styles.menu_logo}>
-                <button>
-                    <List size={32}/>
-                </button>
-                <h1>Logo</h1>
-            </div>
-            <div className={styles.opcoes}>
-              <nav>
-                <a href="">Home</a>
-                <a href="">Ferramentas</a>
-                <a href="">Objetivos</a>
-                <a href="">Bibliografia</a>
-              </nav>
-            </div>
+      <div className={styles.mobile}>
+        <div className={styles.menu_logo}>
+          <button onClick={handleAbrirFecharMenu}>
+            {
+              abrirMenu === true ? (
+                <X size={32} />
+              ) : (
+                <List size={32} />
+              )
+            }
+          </button>
+          <h1>Logo</h1>
         </div>
+
+        <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
+          <nav>
+            <a href="">Home</a>
+            <a href="">Ferramentas</a>
+            <a href="">Objetivos</a>
+            <a href="">Bibliografia</a>
+          </nav>
+        </div>
+      </div>
+
+      <div className={styles.desktop}>
+
+        <div>
+          <h1>Logo</h1>
+        </div>
+
+        <div>
+          <nav className={styles.desktop_options}>
+            <a href="">Home</a>
+            <a href="">Ferramentas</a>
+            <a href="">Objetivos</a>
+            <a href="">Bibliografia</a>
+          </nav>
+        </div>
+
+      </div>
     </header>
   )
 }
